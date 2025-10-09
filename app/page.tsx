5a0bc099cdc1004c3c7d14c64f8ad11256f4ccf0
ayaanqui <ayaanqui.com@gmail.com>
2025-10-09 00:54:40 -0500

    feat: use cdn for images

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ export default function LandingPage() {
         <div className="pointer-events-none hidden md:block">
           <div className="relative rounded-4xl p-2 shadow-2xl transform rotate-6 bg-gray-100">
             <Image
-              src="/screenshots/homepage-main-screenshot.png"
+              src="https://res.cloudinary.com/pricetra-cdn/image/upload/homepage-main-screenshot.png"
               alt="Pricetra Mobile App"
               width={280}
               height={560}
@@ -106,7 +106,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/homepage-category-milk-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/homepage-category-milk-screenshot.png"
                   alt="Pricetra Homepage"
                   width={300}
                   height={600}
@@ -124,7 +124,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/product-page-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/product-page-screenshot.png"
                   alt="Product Details"
                   width={300}
                   height={600}
@@ -142,7 +142,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/add-product-price-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/add-product-price-screenshot.png"
                   alt="Add Price"
                   width={300}
                   height={600}
@@ -160,7 +160,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/product-page-nearby-prices-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/product-page-nearby-prices-screenshot.png"
                   alt="Nearby Prices"
                   width={300}
                   height={600}
@@ -178,7 +178,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/historic-price-data-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/historic-price-data-screenshot.png"
                   alt="Historic Prices"
                   width={300}
                   height={600}
@@ -196,7 +196,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/store-branch-products-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/store-branch-products-screenshot.png"
                   alt="Store Products"
                   width={300}
                   height={600}
@@ -212,7 +212,7 @@ export default function LandingPage() {
             <div className="flex-shrink-0 snap-center w-80">
               <div className="bg-slate-100 rounded-3xl p-4 mb-4">
                 <Image
-                  src="/screenshots/scan-product-screenshot.png"
+                  src="https://res.cloudinary.com/pricetra-cdn/image/upload/scan-product-screenshot.png"
                   alt="Scan Product"
                   width={300}
                   height={600}
