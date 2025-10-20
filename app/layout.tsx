719006fca369e0c5f66f3175f991cf983f4be5ff
ayaanqui <ayaanqui.com@gmail.com>
2025-10-20 18:49:31 -0500

    feat: setup apollo client

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/public/globals.css";
 import Script from "next/script";
+import ApolloWrapper from "@/graphql/ApolloWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -56,7 +57,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
