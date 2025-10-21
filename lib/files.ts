87f619a87cd712b8fe2a26c945275a72fe32c1cb
ayaanqui <ayaanqui.com@gmail.com>
2025-10-20 20:30:37 -0500

    feat: cloudinary util

---
+++ lib/files.ts
@@ -0,0 +1,11 @@
+export const CLOUDINARY_UPLOAD_BASE = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUD_NAME}/image/upload`;
+
+export function createCloudinaryUrl(public_id: string, width?: number, height?: number) {
+  let url = CLOUDINARY_UPLOAD_BASE;
+  const transformations: string[] = [];
+  if (width) transformations.push(`w_${width}`);
+  if (height) transformations.push(`h_${height}`);
+  if (transformations.length > 0) url += `/${transformations.join(',')}`;
+  url += `/${public_id}`;
+  return url;
+}
