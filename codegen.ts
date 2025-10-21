3ae931f88e0571e6ecfb4c6a20bb7b0d51c6f19f
ayaanqui <ayaanqui.com@gmail.com>
2025-10-20 23:11:43 -0500

    fix: gql type generation + re generate

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,19 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: "http://localhost:8080/graphql",
-  documents: "graphql/documents/**/*.ts",
+  schema: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080/graphql',
+  documents: ['graphql/documents/**/*.ts'],
   generates: {
-    "graphql/types/": {
-      preset: "client",
-      plugins: []
+    'graphql/types/': {
+      preset: 'client',
+      // plugins: ['typescript', 'typescript-operations'],
+      config: {
+        // avoidOptionals: true,
+        scalars: {
+          ID: 'number',
+        },
+      },
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+  },
 };
-
 export default config;
