9461a6acfb0c8c8bc427e2a0a5d24a2bfe44fd8e
ayaanqui <ayaanqui.com@gmail.com>
2025-11-11 17:25:47 -0600

    feat: graphql fetcher

---
+++ lib/graphql-client-ssr.ts
@@ -0,0 +1,21 @@
+import { DocumentNode, print } from "graphql";
+
+export async function fetchGraphql<V, R>(DOCUMENT: DocumentNode, type: 'query' | 'mutation', variables?: V, token?: string) {
+  const headers: HeadersInit = {
+    "content-type": "application/json;charset=UTF-8",
+  }
+  if (token) headers['authorization'] = `Bearer ${token}`;
+
+  const body: { [key: string]: unknown } = {}
+  body[type] = print(DOCUMENT);
+  if (variables) {
+    body['variables'] = variables
+  }
+
+  const res = await fetch(process.env.API_URL ?? "https://api.pricetra.com/graphql", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  });
+  return await res.json() as { data?: R, errors?: unknown };
+}
