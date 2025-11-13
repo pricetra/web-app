a0e677ea8d942ad6ce044405b66fe1d820766968
ayaanqui <ayaanqui.com@gmail.com>
2025-11-13 02:56:08 -0600

    fix: use next/image in suspense component

--- components/suspense-fallback.tsx
+++ components/suspense-fallback.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import Image from "next/image";
+
 export function SuspenseFallbackLogo() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-background">
       {/* Animated spinner */}
       <div className="flex flex-col items-center justify-center gap-4">
         <div className="relative size-20 animate-pulse">
-          <img src="/logo_black_color_dark_leaf.svg" alt="logo" />
+          <Image
+            src="/logo_black_color_dark_leaf.svg"
+            alt="logo"
+            width={483}
+            height={344}
+          />
         </div>
       </div>
     </div>
