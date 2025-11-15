d4ba2fa8fdb1836591e0475c915fe49f01dd3e1c
ayaanqui <ayaanqui.com@gmail.com>
2025-11-15 16:33:15 -0600

    feat: product specs section

--- app/products/[productId]/product-page-client.tsx
+++ app/products/[productId]/product-page-client.tsx
@@ -20,6 +20,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import ProductSpecs from "@/components/product-specs";
 
 export type ProductPageClientProps = {
   productId: number;
@@ -97,42 +98,21 @@ export default function ProductPageClient({
             type="multiple"
             defaultChecked
             className="w-full"
-            defaultValue={["item-1"]}
+            defaultValue={["description"]}
           >
-            <AccordionItem value="item-1">
+            <AccordionItem value="description">
               <AccordionTrigger>Description</AccordionTrigger>
               <AccordionContent className="flex flex-col gap-4 text-balance">
                 <p>{productData?.product?.description}</p>
               </AccordionContent>
             </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger>Shipping Details</AccordionTrigger>
+
+            <AccordionItem value="specifications">
+              <AccordionTrigger>Specifications</AccordionTrigger>
               <AccordionContent className="flex flex-col gap-4 text-balance">
-                <p>
-                  We offer worldwide shipping through trusted courier partners.
-                  Standard delivery takes 3-5 business days, while express
-                  shipping ensures delivery within 1-2 business days.
-                </p>
-                <p>
-                  All orders are carefully packaged and fully insured. Track
-                  your shipment in real-time through our dedicated tracking
-                  portal.
-                </p>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>Return Policy</AccordionTrigger>
-              <AccordionContent className="flex flex-col gap-4 text-balance">
-                <p>
-                  We stand behind our products with a comprehensive 30-day
-                  return policy. If you&apos;re not completely satisfied, simply
-                  return the item in its original condition.
-                </p>
-                <p>
-                  Our hassle-free return process includes free return shipping
-                  and full refunds processed within 48 hours of receiving the
-                  returned item.
-                </p>
+                {productData && (
+                  <ProductSpecs product={productData.product as Product} />
+                )}
               </AccordionContent>
             </AccordionItem>
           </Accordion>
---
+++ components/product-specs.tsx
@@ -0,0 +1,38 @@
+import { Product } from '@/graphql/types/graphql';
+
+export type ProductSpecsProps = {
+  product: Product;
+};
+
+export default function ProductSpecs({ product }: ProductSpecsProps) {
+  return (
+    <>
+      {[
+        { name: 'ID', value: product.id.toString() },
+        { name: 'UPC/PLU Code', value: product.code },
+        { name: 'Product Name', value: product.name },
+        { name: 'Brand', value: product.brand },
+        {
+          name: 'Weight',
+          value:
+            product.weightValue && product.weightType
+              ? `${product.weightValue} ${product.weightType}`
+              : 'N/A',
+        },
+        { name: 'Image URL', value: product.image },
+        { name: 'Views', value: product.views.toString() },
+        { name: 'Category', value: product.category?.name },
+        { name: 'Category ID', value: product.categoryId.toString() },
+      ].map(({ name, value }) => (
+        <div
+          className="flex flex-row flex-wrap gap-3 border-b border-gray-100 py-2 last:border-0"
+          key={name}>
+          <h5 className="font-bold">{name}:</h5>
+          <span className="block">
+            {value}
+          </span>
+        </div>
+      ))}
+    </>
+  );
+}
