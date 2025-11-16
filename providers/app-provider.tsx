cad3c472fd35201acdf6dc95f106073314196a0a
ayaanqui <ayaanqui.com@gmail.com>
2025-11-16 11:21:39 -0600

    fix: properly configure day.js extensions

--- providers/app-provider.tsx
+++ providers/app-provider.tsx
@@ -1,9 +1,17 @@
-'use client'
+"use client";
 import LocationContextProvider from "@/context/location-context";
 import { UserContextProvider } from "@/context/user-context";
 import ApolloWrapper from "@/graphql/apollo-wrapper";
 import { ReactNode } from "react";
 import { CookiesProvider } from "react-cookie";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 export type AppProviderProps = {
   children: ReactNode;
