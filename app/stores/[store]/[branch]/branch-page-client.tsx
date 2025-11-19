a1051bf66ea67aba7334d1e1ec01c25837b3b8b3
ayaanqui <ayaanqui.com@gmail.com>
2025-11-19 00:23:24 -0600

    feat: display branch products

--- app/stores/[store]/[branch]/branch-page-client.tsx
+++ app/stores/[store]/[branch]/branch-page-client.tsx
@@ -4,53 +4,65 @@ import { useNavbar } from "@/context/navbar-context";
 import {
   AllProductsDocument,
   Branch,
+  Product,
   Store,
 } from "@/graphql/types/graphql";
 import { createCloudinaryUrl } from "@/lib/files";
 import { useLayoutEffect } from "react";
 import Image from "next/image";
 import { useQuery } from "@apollo/client/react";
+import ProductItem from "@/components/product-item";
+import Link from "next/link";
 
-export default function BranchPageClient({ store, branch }: { store: Store, branch: Branch }) {
+export default function BranchPageClient({
+  store,
+  branch,
+}: {
+  store: Store;
+  branch: Branch;
+}) {
   const { setPageIndicator, resetAll } = useNavbar();
 
-  const {} = useQuery(
-    AllProductsDocument,
-    {
-      fetchPolicy: "no-cache",
-      variables: {
-        paginator: {
-          page: 1,
-          limit: 10,
-        },
-        search: {
-          storeId: store.id,
-          branchId: branch.id,
-        },
+  const { data: productsData } = useQuery(AllProductsDocument, {
+    fetchPolicy: "no-cache",
+    variables: {
+      paginator: {
+        page: 1,
+        limit: 50,
       },
-    }
-  );
+      search: {
+        storeId: store.id,
+        branchId: branch.id,
+      },
+    },
+  });
 
   useLayoutEffect(() => {
     setPageIndicator(
       <>
-        <Image
-          alt={store.name}
-          src={createCloudinaryUrl(store.logo, 100, 100)}
-          width={100}
-          height={100}
-          quality={100}
-          className="flex size-[30px] items-center justify-center rounded-sm bg-white"
-        />
+        <Link href={`/stores/${store.slug}`} className="block">
+          <Image
+            alt={store.name}
+            src={createCloudinaryUrl(store.logo, 100, 100)}
+            width={100}
+            height={100}
+            quality={100}
+            className="flex size-[30px] items-center justify-center rounded-sm bg-white"
+          />
+        </Link>
 
         <div className="flex flex-col gap-1">
-          <h2 className="font-bold line-clamp-1 break-all flex-1 sm:text-sm text-xs leading-none">
-            {store.name}
-          </h2>
+          <Link href={`/stores/${store.slug}`}>
+            <h2 className="font-bold line-clamp-1 break-all flex-1 sm:text-sm text-xs leading-none">
+              {store.name}
+            </h2>
+          </Link>
 
-          <p className="break-all line-clamp-1 text-[10px] leading-none">
-            {branch.address.fullAddress}
-          </p>
+          <a href={branch.address.mapsLink} className="block">
+            <span className="break-all line-clamp-1 text-[10px] leading-none block hover:underline">
+              {branch.address.fullAddress}
+            </span>
+          </a>
         </div>
       </>
     );
@@ -62,8 +74,12 @@ export default function BranchPageClient({ store, branch }: { store: Store, bran
   }, []);
 
   return (
-    <div className="mt-0 sm:mt-10">
-      
+    <div className="w-full max-w-[1000px] mt-0 sm:mt-10 px-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-7 gap-x-3">
+        {productsData?.allProducts?.products?.map((p, i) => (
+          <ProductItem product={p as Product} key={`product-${p.id}-${i}`} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ components/product-item.tsx
@@ -0,0 +1,148 @@
+import { Product } from "@/graphql/types/graphql";
+import useCalculatedPrice from "@/hooks/useCalculatedPrice";
+import useIsSaleExpired from "@/hooks/useIsSaleExpired";
+import useProductWeightBuilder from "@/hooks/useProductWeightBuilder";
+import { createCloudinaryUrl } from "@/lib/files";
+import { currencyFormat, getPriceUnit } from "@/lib/strings";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import ProductMetadataBadge from "./product-metadata-badge";
+import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+
+export type ProductItemOptionalProps = {
+  imgWidth?: number;
+  hideStoreInfo?: boolean;
+  hideAddButton?: boolean;
+};
+
+export type ProductItemProps = ProductItemOptionalProps & {
+  product: Product;
+};
+
+export default function ProductItem({
+  product,
+  hideStoreInfo = false,
+  imgWidth = 130,
+}: // hideAddButton = false,
+ProductItemProps) {
+  const isExpired = useIsSaleExpired(product.stock?.latestPrice);
+  const calculatedAmount = useCalculatedPrice({
+    isExpired,
+    latestPrice: product.stock?.latestPrice,
+  });
+  const weight = useProductWeightBuilder(product);
+
+  return (
+    <Link
+      href={`/products/${product.id}?stockId=${product.stock?.id}`}
+      className="flex max-w-full flex-row gap-2"
+    >
+      <div style={{ width: imgWidth, height: imgWidth, position: "relative" }}>
+        {product.stock?.latestPrice?.sale && !isExpired && (
+          <div className="absolute left-1 top-1 z-[1] w-[40px]">
+            <span className="inline-block rounded-sm bg-red-700 px-1.5 py-2 text-center text-[9px] font-bold text-white leading-0">
+              SALE
+            </span>
+          </div>
+        )}
+
+        <Image
+          src={createCloudinaryUrl(product.code, 500)}
+          alt={product.code}
+          className="rounded-xl"
+          style={{ width: imgWidth, height: imgWidth }}
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="flex max-w-full flex-1 flex-col gap-3 px-2">
+        <div className="flex flex-col gap-1">
+          <div className="mb-1 flex flex-row items-center gap-1">
+            {product.weightValue && product.weightType && (
+              <ProductMetadataBadge type="weight" size="sm" text={weight} />
+            )}
+            {product.quantityValue && product.quantityType && (
+              <ProductMetadataBadge
+                type="quantity"
+                size="sm"
+                text={`${product.quantityValue} ${product.quantityType}`}
+              />
+            )}
+          </div>
+
+          <div className="flex flex-row flex-wrap items-center gap-1">
+            {product.brand && product.brand !== "N/A" && (
+              <h4 className="text-xs text-gray-600">{product.brand}</h4>
+            )}
+          </div>
+
+          <Link href={`/products/${product.id}?stockId=${product.stock?.id}`}>
+            <h3 className="line-clamp-3">{product.name}</h3>
+          </Link>
+        </div>
+
+        {product.stock && (
+          <div className="flex flex-row items-center justify-between gap-2">
+            {/* {!hideStoreInfo && product.stock.store && product.stock.branch && (
+              <div className="flex-[2] gap-1">
+                <ProductStockMini stock={product.stock} />
+              </div>
+            )} */}
+
+            {product.stock.latestPrice && (
+              <div
+                className={cn(
+                  "flex-[1] flex-col",
+                  hideStoreInfo ? "items-start" : "items-end"
+                )}
+              >
+                {product.stock.latestPrice.sale &&
+                  !isExpired &&
+                  product.stock.latestPrice.originalPrice && (
+                    <span className="text-right text-xs line-through text-red-700">
+                      {currencyFormat(product.stock.latestPrice.originalPrice)}
+                    </span>
+                  )}
+                <div className="flex flex-row items-center justify-start gap-1">
+                  <span className="font-black">
+                    {currencyFormat(calculatedAmount)}
+                  </span>
+                  {product.stock.latestPrice.unitType !== "item" && (
+                    <span className="text-xs text-gray-500">
+                      {getPriceUnit(product.stock.latestPrice)}
+                    </span>
+                  )}
+                </div>
+                {product.quantityValue > 1 && (
+                  <span className="text-right text-[10px] text-gray-500">
+                    {`${currencyFormat(
+                      calculatedAmount / product.quantityValue
+                    )}/${product.quantityType}`}
+                  </span>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </Link>
+  );
+}
+
+export function ProductItemLoading({
+  imgWidth = 130,
+}: Pick<ProductItemProps, "imgWidth">) {
+  return (
+    <div className="flex max-w-full flex-row gap-2">
+      <div style={{ width: imgWidth, height: imgWidth }}>
+        <Skeleton className="size-full rounded-xl" />
+      </div>
+      <div className="max-w-full flex-1 gap-2 p-2">
+        <Skeleton className="h-6 w-full" />
+        <Skeleton className="h-6 w-full" />
+        <Skeleton className="h-6 w-[100px]" />
+      </div>
+    </div>
+  );
+}
