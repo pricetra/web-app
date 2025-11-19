188ab7e0f4c6f25e0ca8d5d6fe4bc560314dbd6a
ayaanqui <ayaanqui.com@gmail.com>
2025-11-18 18:57:35 -0600

    feat: show store branches with products

--- app/stores/[store]/selected-store-page-client.tsx
+++ app/stores/[store]/selected-store-page-client.tsx
@@ -1,13 +1,47 @@
 "use client";
 
 import { useNavbar } from "@/context/navbar-context";
-import { Store } from "@/graphql/types/graphql";
+import {
+  Branch,
+  BranchesWithProductsDocument,
+  Product,
+  Store,
+} from "@/graphql/types/graphql";
 import { createCloudinaryUrl } from "@/lib/files";
 import { useLayoutEffect } from "react";
 import Image from "next/image";
+import { useQuery } from "@apollo/client/react";
+import BranchItemWithLogo, {
+  BranchItemWithLogoLoading,
+} from "@/components/branch-item-with-logo";
+import ProductItemHorizontal, {
+  ProductLoadingItemHorizontal,
+} from "@/components/product-item-horizontal";
+import useLocationInput from "@/hooks/useLocationInput";
 
 export default function SelectedStorePageClient({ store }: { store: Store }) {
   const { setPageIndicator, resetAll } = useNavbar();
+  const location = useLocationInput();
+
+  const { data: branchesWithProducts } = useQuery(
+    BranchesWithProductsDocument,
+    {
+      fetchPolicy: "no-cache",
+      variables: {
+        paginator: {
+          page: 1,
+          limit: 10,
+        },
+        productLimit: 10,
+        filters: {
+          storeId: store.id,
+          location: location
+            ? { ...location.locationInput, radiusMeters: undefined }
+            : undefined,
+        },
+      },
+    }
+  );
 
   useLayoutEffect(() => {
     setPageIndicator(
@@ -30,7 +64,55 @@ export default function SelectedStorePageClient({ store }: { store: Store }) {
     return () => {
       resetAll();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div className="px-5 mt-0 sm:mt-10"></div>;
+  return (
+    <div className="mt-0 sm:mt-10">
+      {!branchesWithProducts
+        ? Array(3)
+            .fill(0)
+            .map((_, i) => (
+              <article
+                className="my-7"
+                key={`branch-with-product-loading-${i}`}
+              >
+                <div className="mb-5 px-5">
+                  <BranchItemWithLogoLoading />
+                </div>
+
+                <div className="flex flex-row gap-5 overflow-x-auto py-2.5 lg:px-2.5 lg:[mask-image:_linear-gradient(to_right,transparent_0,_black_2em,_black_calc(100%-2em),transparent_100%)]">
+                  {Array(10)
+                    .fill(0)
+                    .map((_, j) => (
+                      <div
+                        className="first:pl-5 last:pr-5"
+                        key={`branch-product-${i}-${j}`}
+                      >
+                        <ProductLoadingItemHorizontal />
+                      </div>
+                    ))}
+                </div>
+              </article>
+            ))
+        : branchesWithProducts.branchesWithProducts.branches.map((branch) => (
+            <article className="my-7" key={`branch-with-product-${branch.id}`}>
+              <div className="mb-5 px-5">
+                <BranchItemWithLogo branch={branch as Branch} cityName />
+              </div>
+
+              <div className="flex flex-row gap-5 overflow-x-auto py-2.5 lg:px-2.5 lg:[mask-image:_linear-gradient(to_right,transparent_0,_black_2em,_black_calc(100%-2em),transparent_100%)]">
+                {(branch.products ?? []).map((product) => (
+                  <div
+                    className="first:pl-5 last:pr-5"
+                    key={`branch-product-${branch.id}-${product.id}`}
+                  >
+                    <ProductItemHorizontal product={product as Product} />
+                  </div>
+                ))}
+              </div>
+            </article>
+          ))}
+    </div>
+  );
 }
--- components/branch-item-with-logo.tsx
+++ components/branch-item-with-logo.tsx
@@ -2,29 +2,44 @@ import { Branch } from '@/graphql/types/graphql';
 import { createCloudinaryUrl } from '@/lib/files';
 import { metersToMiles } from '@/lib/utils';
 import Image from "next/image"
+import { useMemo } from "react";
 import Skeleton from "react-loading-skeleton";
 
 export type BranchItemWithLogoProps = {
   branch: Branch;
+  hideStoreLogo?: boolean;
+  branchName?: boolean;
+  cityName?: boolean;
 };
 
 export default function BranchItemWithLogo({
   branch,
+  hideStoreLogo = false,
+  branchName = false,
+  cityName = false,
 }: BranchItemWithLogoProps) {
+  const name = useMemo(() => {
+    if (branchName) return branch.name;
+    if (cityName && branch.address) return branch.address.city;
+    return branch.store?.name ?? branch.name;
+  }, [branch, cityName, branchName]);
+
   return (
     <div className="flex flex-row justify-between gap-5">
       <div className="flex flex-1 flex-row gap-4">
-        <Image
-          src={createCloudinaryUrl(branch.store?.logo ?? "", 500, 500)}
-          className="size-[40px] sm:size-[50px] rounded-lg border-[1px] border-gray-200"
-          width={500}
-          height={500}
-          alt={branch.name}
-        />
+        {!hideStoreLogo && (
+          <Image
+            src={createCloudinaryUrl(branch.store?.logo ?? "", 500, 500)}
+            className="size-[40px] sm:size-[50px] rounded-lg border-[1px] border-gray-200"
+            width={500}
+            height={500}
+            alt={branch.name}
+          />
+        )}
         <div className="flex flex-col pr-[60px]">
           <div className="flex w-full flex-row flex-nowrap items-center gap-x-3">
             <h5 className="sm:text-lg font-bold line-clamp-1 break-all">
-              {branch.store?.name}
+              {name}
             </h5>
 
             {branch.address?.distance && (
@@ -45,14 +60,20 @@ export default function BranchItemWithLogo({
   );
 }
 
-export function BranchItemWithLogoLoading() {
+export function BranchItemWithLogoLoading({
+  hideStoreLogo = false,
+}: {
+  hideStoreLogo?: boolean;
+}) {
   return (
     <div className="flex flex-row justify-between gap-5">
       <div className="flex flex-1 flex-row gap-4">
-        <Skeleton
-          className="size-[40px] rounded-xl"
-          style={{ width: 40, borderRadius: 7 }}
-        />
+        {!hideStoreLogo && (
+          <Skeleton
+            className="size-[40px] rounded-xl"
+            style={{ width: 40, borderRadius: 7 }}
+          />
+        )}
         <div className="flex flex-row pr-[60px] flex-wrap">
           <div className="flex w-full flex-row flex-wrap items-center gap-3">
             <Skeleton
