91075bc2296f45eb75484a8ed0a5854f7c724f65
ayaanqui <ayaanqui.com@gmail.com>
2025-12-05 00:32:18 -0600

    fix: modal responsive styles

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -43,8 +43,8 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="w-full p-5 flex flex-row items-center justify-center">
-        <div className="z-50 grid w-full max-w-lg gap-4 border bg-background p-6 shadow-lg sm:rounded-lg relative">
+      <div className="w-full min-h-full p-0 xs:p-5 flex flex-row items-center justify-center">
+        <div className="z-50 grid w-full xs:max-w-lg gap-4 border bg-background p-6 shadow-lg xs:rounded-lg relative">
           {children}
           <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
             <X className="h-4 w-4" />
@@ -55,20 +55,14 @@ const DialogContent = React.forwardRef<
     </DialogPrimitive.Content>
   </DialogPortal>
 ));
-DialogContent.displayName = DialogPrimitive.Content.displayName
+DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
-      className
-    )}
-    {...props}
-  />
-)
+  <div className={cn("flex flex-col space-y-1.5", className)} {...props} />
+);
 DialogHeader.displayName = "DialogHeader"
 
 const DialogFooter = ({
