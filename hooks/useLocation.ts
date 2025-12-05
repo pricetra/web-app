1487843957727e037d076d544234ed0d072188a5
ayaanqui <ayaanqui.com@gmail.com>
2025-12-05 01:42:20 -0600

    feat: useLocationService

---
+++ hooks/useLocation.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export type GeolocationCallback = (l?: GeolocationPosition) => void
+
+export default function useLocationService() {
+  const [permissionGranted, setPermissionGranted] = useState(false);
+  const [location, setLocation] = useState<GeolocationPosition>();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((l) => {
+      setPermissionGranted(true);
+      setLocation(l)
+    }, () => {
+      setPermissionGranted(false);
+      setLocation(undefined)
+    });
+  }, []);
+
+  function getCurrentGeocodeAddress() {
+    navigator.geolocation.getCurrentPosition((l) => {
+      setPermissionGranted(true);
+      setLocation(l)
+    }, () => {
+      setPermissionGranted(false);
+      setLocation(undefined)
+    });
+  }
+
+  function watchLocation(cb: GeolocationCallback) {
+    navigator.geolocation.watchPosition((l) => {
+      cb(l);
+      setLocation(l);
+    }, () => {
+      cb(undefined);
+      setLocation(undefined);
+    })
+  }
+
+  return {
+    location,
+    permissionGranted,
+    watchLocation,
+    getCurrentGeocodeAddress,
+  };
+}
