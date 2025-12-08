f4159e26bef3da99ad2edd30795cf80548e380ff
ayaanqui <ayaanqui.com@gmail.com>
2025-12-07 20:48:46 -0600

    feat: fetch and include all stores, categories, and brands in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,52 +1,126 @@
-import { MetadataRoute } from 'next'
+import { createCloudinaryUrl } from "@/lib/files";
+import { fetchGraphql } from "@/lib/graphql-client-ssr";
+import {
+  AllStoresDocument,
+  AllStoresQuery,
+  AllStoresQueryVariables,
+  CategorySearchDocument,
+  CategorySearchQueryVariables,
+  CategorySearchQuery,
+  AllBrandsDocument,
+  AllBrandsQueryVariables,
+  AllBrandsQuery,
+} from "graphql-utils";
+import { MetadataRoute } from "next";
+import { cache } from "react";
 
-const URL = 'https://pricetra.com'
+const URL = "https://pricetra.com";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+const getAllStores = cache(() =>
+  fetchGraphql<AllStoresQueryVariables, AllStoresQuery>(
+    AllStoresDocument,
+    "query",
+    {
+      paginator: {
+        limit: 500,
+        page: 1,
+      },
+    }
+  )
+);
+
+const getAllCategories = cache(() =>
+  fetchGraphql<CategorySearchQueryVariables, CategorySearchQuery>(
+    CategorySearchDocument,
+    "query",
+    {
+      search: "",
+    }
+  )
+);
+
+const getAllBrands = cache(() =>
+  fetchGraphql<AllBrandsQueryVariables, AllBrandsQuery>(
+    AllBrandsDocument,
+    "query",
+    {}
+  )
+);
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { data: allStoresData } = await getAllStores();
+  const stores = allStoresData?.allStores?.stores ?? [];
+
+  const { data: allCategoriesData } = await getAllCategories();
+  const categories = allCategoriesData?.categorySearch ?? [];
+
+  const { data: allBrandsData } = await getAllBrands();
+  const brands = allBrandsData?.allBrands ?? [];
+
   return [
     {
       url: URL,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${URL}/home`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "always",
       priority: 1,
     },
     {
       url: `${URL}/search`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "always",
       priority: 1,
     },
     {
       url: `${URL}/auth/login`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${URL}/auth/signup`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${URL}/stores`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.9,
     },
-    // TODO: query all stores instead... paginatorInput: {limit: 1000, page: 1}
-    {
-      url: `${URL}/stores?page=2`,
+
+    // stores
+    ...(stores.map((s) => ({
+      url: `${URL}/stores/${s.slug}`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
+      changeFrequency: "always",
+      priority: 0.9,
+      images: [createCloudinaryUrl(s.logo)],
+    })) as MetadataRoute.Sitemap),
+
+    // categories
+    ...(categories.map((c) => ({
+      url: `${URL}/search?categoryId=${c.id}&amp;category=${encodeURIComponent(
+        c.name
+      )}`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    })) as MetadataRoute.Sitemap),
+
+    // brands
+    ...(brands.map((b) => ({
+      url: `${URL}/search?brand=${encodeURIComponent(b.brand)}`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    })) as MetadataRoute.Sitemap),
     // {
     //   url: `${URL}/categories`,
     //   lastModified: new Date(),
@@ -59,5 +133,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
     //   changeFrequency: 'weekly',
     //   priority: 0.9,
     // },
-  ]
+  ];
 }
