ff1779edfb32af8a790417714b725e161ab0639f
ayaanqui <ayaanqui.com@gmail.com>
2025-12-09 17:44:04 -0600

    feat: welcome screen animation

--- providers/setup-provider.tsx
+++ providers/setup-provider.tsx
@@ -1,16 +1,27 @@
 import WelcomeScreen from "@/components/welcome-screen";
 import { useAuth } from "@/context/user-context";
 import { ReactNode } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function SetupProvider({ children }: { children: ReactNode }) {
   const { user, lists } = useAuth();
-  const noFavoriteBranches = (lists?.favorites?.branchList ?? []).length === 0
+  const noFavoriteBranches = (lists?.favorites?.branchList ?? []).length === 0;
+  const showWelcomeScreen = user && (!user?.address || noFavoriteBranches);
 
-  if (user && (!user?.address || noFavoriteBranches)) {
+  if (showWelcomeScreen) {
     return (
-      <div className="h-screen w-full absolute top-0 left-0 z-50 bg-white overflow-scroll">
-        <WelcomeScreen user={user} />
-      </div>
+      <AnimatePresence>
+        <motion.div
+          key="welcome-screen"
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "100%" }}
+          transition={{ stiffness: 70 }}
+          className="h-screen w-full absolute top-0 left-0 z-50 bg-white overflow-scroll"
+        >
+          <WelcomeScreen user={user} />
+        </motion.div>
+      </AnimatePresence>
     );
   }
 
