a12859470eec2ad2d8341dcf950687d0e029c11d
ayaanqui <ayaanqui.com@gmail.com>
2025-12-09 19:28:48 -0600

    fix: deps

--- app/search/search-page-client.tsx
+++ app/search/search-page-client.tsx
@@ -83,6 +83,7 @@ export default function SearchPageClient({
     return () => {
       setPageIndicator(undefined);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
