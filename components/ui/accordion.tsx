999e72305468307b1694aa06dcd465f6a3a94d02
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2025-12-31 04:05:06 -0600

    fix: accordion arrow toggle

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDown } from "lucide-react"
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDown } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Accordion = AccordionPrimitive.Root
+const Accordion = AccordionPrimitive.Root;
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
@@ -30,27 +30,29 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 text-base sm:text-lg font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180 cursor-pointer",
+        "flex flex-1 items-center justify-between py-4 text-base sm:text-lg font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180 cursor-pointer gap-3",
         className
       )}
       {...props}
     >
-      {children}
+      <div className="flex-2 flex flex-row items-center justify-between gap-3">
+        <div className="flex-1">{children}</div>
 
-      <div className="flex flex-row items-center gap-3">
-        {props.badge ? (
-          <span className="px-1.5 sm:px-2 py-2 sm:py-2.5 bg-pricetra-green-heavy-dark text-white rounded-full text-[9px] sm:text-xs leading-0">
-            {props.badge}
-          </span>
-        ) : (
-          <></>
-        )}
-        <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+        <div className="flex-0">
+          {props.badge ? (
+            <span className="px-1.5 sm:px-2 py-0.5 sm:py-1 bg-pricetra-green-heavy-dark text-white rounded-full text-[9px] sm:text-xs leading-0">
+              {props.badge}
+            </span>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
@@ -63,7 +65,7 @@ const AccordionContent = React.forwardRef<
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
-))
-AccordionContent.displayName = AccordionPrimitive.Content.displayName
+));
+AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
