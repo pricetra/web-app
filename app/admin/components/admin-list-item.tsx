e8a246cc86fb33d85d68419578bf5f6d58633cf6
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-01-17 03:48:38 -0600

    ft/Product view admin page (#13)

---
+++ app/admin/components/admin-list-item.tsx
@@ -0,0 +1,37 @@
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { ReactNode } from "react";
+import { FaAngleRight } from "react-icons/fa6";
+import Link from "next/link";
+
+export type AdminListItemProps = {
+  href: string;
+  title: string;
+  content: string;
+  icon: ReactNode;
+};
+
+export default function AdminListItem({ title, content, icon, href }: AdminListItemProps) {
+  return (
+    <Item variant="outline" size="sm" asChild>
+      <Link href={href}>
+        <ItemMedia>
+          <div className="text-xl">{icon}</div>
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>{title}</ItemTitle>
+          <ItemDescription>{content}</ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <FaAngleRight className="size-4" />
+        </ItemActions>
+      </Link>
+    </Item>
+  );
+}
