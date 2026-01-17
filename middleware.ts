405d28dff8a15774144250ae40bab026f740d52c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-01-17 03:41:17 -0600

    security: block bytedance

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { AUTH_TOKEN_KEY } from "@/lib/cookies";
 import { adminAuthorize, authorize } from "@/lib/roles";
 
+const blockedAgents = ["bytedance", "bytespider"];
+
 export default async function middleware(req: NextRequest) {
-  const curPath = req.nextUrl.pathname;
-  const authToken = req.cookies.get(AUTH_TOKEN_KEY)?.value
-  if (curPath.startsWith('/admin')) {
-    const authUser = await adminAuthorize(authToken);
-    if (!authUser) return NextResponse.redirect(new URL('/', req.url));
+  const userAgent = req.headers.get("user-agent");
+  if (userAgent) {
+    const agent = userAgent.toLowerCase();
+    if (blockedAgents.some((l) => agent.includes(l))) {
+      return new NextResponse("Bot access forbidden", { status: 403 });
+    }
   }
 
-  if (curPath.startsWith('/profile')) {
+  const curPath = req.nextUrl.pathname;
+  const authToken = req.cookies.get(AUTH_TOKEN_KEY)?.value;
+  if (curPath.startsWith("/admin")) {
+    const authUser = await adminAuthorize(authToken);
+    if (!authUser) return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (curPath.startsWith("/profile")) {
     const authUser = await authorize(authToken);
-    if (!authUser) return NextResponse.redirect(new URL('/', req.url));
+    if (!authUser) return NextResponse.redirect(new URL("/", req.url));
   }
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/profile/:path*'],
-}
+  matcher: ["/:path*", "/admin/:path*", "/profile/:path*"],
+};
