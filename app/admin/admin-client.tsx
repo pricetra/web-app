67234993e80985a0b31c8a00aef77dad13b63e54
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-01-17 01:44:03 -0600

    refactor: admin list item

--- app/admin/admin-client.tsx
+++ app/admin/admin-client.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemMedia,
-  ItemTitle,
-} from "@/components/ui/item";
 import NavPageIndicator from "@/components/ui/nav-page-indicator";
 import { useNavbar } from "@/context/navbar-context";
 import { useLayoutEffect } from "react";
 import { RiAdminLine } from "react-icons/ri";
-import { FaAngleRight } from "react-icons/fa6";
 import { BsPersonFillAdd } from "react-icons/bs";
 import { MdStorefront } from "react-icons/md";
-import Link from "next/link";
+import AdminListItem from "./components/admin-list-item";
 
 export default function AdminClient() {
   const { setPageIndicator, resetAll } = useNavbar();
@@ -33,37 +24,19 @@ export default function AdminClient() {
     <>
       <div className="w-full max-w-[1000px] flex-1 px-5">
         <div className="flex flex-col gap-5">
-          <Item variant="outline" size="sm" asChild>
-            <Link href="/admin/stores/new">
-              <ItemMedia>
-                <MdStorefront className="size-5" />
-              </ItemMedia>
-              <ItemContent>
-                <ItemTitle>Add New Store</ItemTitle>
-                <ItemDescription>Create a new store entry</ItemDescription>
-              </ItemContent>
-              <ItemActions>
-                <FaAngleRight className="size-4" />
-              </ItemActions>
-            </Link>
-          </Item>
+          <AdminListItem
+            href="/admin/stores/new"
+            icon={<MdStorefront />}
+            title="Add New Store"
+            content="Create a new store entry"
+          />
 
-          <Item variant="outline" size="sm" asChild>
-            <Link href="/admin/stores/store-users/add">
-              <ItemMedia>
-                <BsPersonFillAdd className="size-5" />
-              </ItemMedia>
-              <ItemContent>
-                <ItemTitle>Add Store User</ItemTitle>
-                <ItemDescription>
-                  Send a join request to a new user to an existing store
-                </ItemDescription>
-              </ItemContent>
-              <ItemActions>
-                <FaAngleRight className="size-4" />
-              </ItemActions>
-            </Link>
-          </Item>
+          <AdminListItem
+            href="/admin/stores/store-users/add"
+            icon={<BsPersonFillAdd />}
+            title="Add Store User"
+            content="Send a join request to a new user to an existing store"
+          />
         </div>
       </div>
     </>
