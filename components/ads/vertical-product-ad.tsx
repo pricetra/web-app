53532163808c65284ed522391af7b7218b5b4bd9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-01-29 22:36:04 -0600

    fix: vertical ad

--- components/ads/vertical-product-ad.tsx
+++ components/ads/vertical-product-ad.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { GADS_PUB_ID } from "@/constants/google";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export type VerticalProductAdProps = {
   id?: string | number;
@@ -16,21 +16,33 @@ declare global {
 export default function VerticalProductAd({ id }: VerticalProductAdProps) {
   const adRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    if (!adRef.current) return;
+
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.warn("AdSense error", e);
+    }
+  }, []);
+
   return (
-    <div
-      ref={adRef}
-      className="relative w-full"
-      style={{ maxWidth: 474, minHeight: 100 }}
-      id={`horizontal-product-ad-${id}`}
-    >
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-format="fluid"
-        data-ad-layout-key="-h4-g+2e-7p+al"
-        data-ad-client={GADS_PUB_ID}
-        data-ad-slot="2926382943"
-      />
+    <div className="">
+      <div
+        ref={adRef}
+        className="relative w-full"
+        style={{ maxWidth: 474 }}
+        id={`horizontal-product-ad-${id}`}
+      >
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-format="fluid"
+          data-ad-layout-key="-h4-g+2e-7p+al"
+          data-ad-client={GADS_PUB_ID}
+          data-ad-slot="2926382943"
+        />
+      </div>
     </div>
   );
 }
