811bf058d98714cc27dae513c3c2517440b02fe0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-02-10 13:43:20 -0600

    style: branding section responsive gap

--- components/ui/navbar-main.tsx
+++ components/ui/navbar-main.tsx
@@ -99,7 +99,7 @@ export default function NavbarMain() {
         >
           <div
             className={cn(
-              "flex flex-row gap-1 sm:gap-3 md:gap-6 items-center justify-start flex-1 w-full pl-5 pr-0",
+              "flex flex-row gap-1 sm:gap-3 lg:gap-6 items-center justify-start flex-1 w-full pl-5 pr-0",
               searchPanelOpen || navTools ? "max-w-full" : "max-w-4xl"
             )}
           >
