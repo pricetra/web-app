15a0230b643d7e94cf4fdadb64dbab7e924ca70b
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-02-12 01:34:52 -0600

    feat: custom link component

---
+++ components/ui/link.tsx
@@ -0,0 +1,30 @@
+import { LinkProps } from "next/link";
+import NProgress from "nprogress";
+import { AnchorHTMLAttributes, ReactNode } from "react";
+
+export type CustomLinkType = AnchorHTMLAttributes<HTMLAnchorElement> &
+  LinkProps & {
+    children?: ReactNode | undefined;
+  };
+
+export default function Link({ onClick, ...props }: CustomLinkType) {
+  return (
+    <Link
+      onClick={(e) => {
+        if (
+          e.metaKey ||
+          e.ctrlKey ||
+          e.shiftKey ||
+          e.altKey ||
+          e.button !== 0
+        ) {
+          return;
+        }
+
+        NProgress.start();
+        if (onClick) onClick(e);
+      }}
+      {...props}
+    />
+  );
+}
