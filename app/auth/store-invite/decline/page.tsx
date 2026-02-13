3ce0c1a9950e3aeef1837068840eee48a0ff0576
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-02-13 01:53:17 -0600

    refactor: base64 object parser

--- app/auth/store-invite/decline/page.tsx
+++ app/auth/store-invite/decline/page.tsx
@@ -3,6 +3,7 @@ import { StoreUserData } from "graphql-utils";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import DeclineInviteClient from "./client";
+import { parseBase64StringToObject } from "@/lib/strings";
 
 type Props = {
   searchParams: Promise<{ data?: string }>;
@@ -18,11 +19,9 @@ export default async function StoreUserInviteDeclinePage(props: Props) {
   const searchParams = await props.searchParams;
   if (!searchParams.data || searchParams.data === "") redirect("/");
 
-  const bufferDataObj = Buffer.from(searchParams.data, "base64");
-  const decodedRawDataObj = bufferDataObj.toString("utf-8");
   let parsedData = {} as StoreUserData;
   try {
-    parsedData = JSON.parse(decodedRawDataObj);
+    parsedData = parseBase64StringToObject<StoreUserData>(searchParams.data);
   } catch {
     redirect("/");
   }
