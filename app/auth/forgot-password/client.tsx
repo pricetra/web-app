df530d0e6a84f04cb86e905408b4f2b80725fe70
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-02-13 22:46:47 -0600

    feat: forgot password functionality

--- app/auth/forgot-password/client.tsx
+++ app/auth/forgot-password/client.tsx
@@ -1,8 +1,14 @@
+"use client";
 import AuthContainer from "@/components/auth/auth-container";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 import { Label } from "@/components/ui/label";
-import Link from "@/components/ui/link";
 import { useLazyQuery, useMutation } from "@apollo/client/react";
 import {
   RequestResetPasswordDocument,
@@ -10,8 +16,12 @@ import {
   VerifyPasswordResetCodeDocument,
 } from "graphql-utils";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { BsEnvelopeCheck } from "react-icons/bs";
+import { toast } from "sonner";
+import NProgress from "nprogress";
+
+const CELL_COUNT = 6;
 
 export default function ForgotPasswordClient() {
   const router = useRouter();
@@ -45,10 +55,47 @@ export default function ForgotPasswordClient() {
   const loading = requestCodeLoading || verificationLoading || resetting;
   const error = requestCodeError || verificationError || resetError;
 
+  const paramsBuilder = useMemo(() => {
+    const paramsBuilder = new URLSearchParams();
+    if (email) {
+      paramsBuilder.set("email", email);
+    }
+    if (returnPath) {
+      paramsBuilder.set("return", returnPath);
+    }
+    return paramsBuilder;
+  }, [email, returnPath]);
+
   useEffect(() => {
     if (email && email.length > 1) return;
-    router.push("/auth/signup");
-  }, [email, router]);
+    router.push(`/auth/login?${paramsBuilder.toString()}`);
+  }, [email, router, paramsBuilder]);
+
+  useEffect(() => {
+    if (!requestCodeError) return;
+
+    toast.error(
+      "Could not send reset email. Perhaps the email you entered was incorrect?",
+    );
+    router.push(`/auth/login?${paramsBuilder.toString()}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [requestCodeError, paramsBuilder]);
+
+  useEffect(() => {
+    if (!email) return;
+
+    NProgress.start();
+    requestCode({ variables: { email } }).finally(() => NProgress.done());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email]);
+
+  useEffect(() => {
+    if (!resetData?.updatePasswordWithResetCode) return;
+
+    toast.success("Password updated! Please use the new password to login");
+    router.push(`/auth/login?${paramsBuilder.toString()}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resetData, paramsBuilder]);
 
   if (!email) return <></>;
 
@@ -69,37 +116,62 @@ export default function ForgotPasswordClient() {
       extras={
         <div className="text-center text-sm">
           Didn&apos;t get an email?{" "}
-          <Link
-            href="#"
-            className="underline underline-offset-4"
+          <Button
+            variant="link"
+            className="underline underline-offset-4 px-1"
             onClick={() => requestCode({ variables: { email } })}
           >
             Resend reset code
-          </Link>
+          </Button>
         </div>
       }
     >
-      {!error && email && (
+      {!requestCodeError && requestCodeData?.requestPasswordReset && (
         <Alert variant="success">
           <BsEnvelopeCheck />
-          <AlertTitle>Verification email sent!</AlertTitle>
+          <AlertTitle>Password reset email sent!</AlertTitle>
           <AlertDescription>
-            An email with the verification code was sent to <i>{email}</i>
+            Your password reset code was sent to <i>{email}</i>
           </AlertDescription>
         </Alert>
       )}
 
       <div className="grid gap-2">
-        <Label htmlFor="email">Code</Label>
-        <Input
-          id="code"
-          type="number"
-          placeholder="Verification code"
-          required
-          value={verificationCode}
-          onChange={(v) => setVerificationCode(v.target.value)}
-          disabled={verifyLoading}
-        />
+        {!verificationData ? (
+          <>
+            <Label htmlFor="code">Code</Label>
+
+            <div className="flex flex-row items-center justify-center">
+              <InputOTP
+                maxLength={CELL_COUNT}
+                value={code}
+                onChange={(value) => setCode(value)}
+                id="code"
+              >
+                <InputOTPGroup>
+                  {Array(CELL_COUNT)
+                    .fill(0)
+                    .map((_, i) => (
+                      <InputOTPSlot index={i} key={`code-${i}`} />
+                    ))}
+                </InputOTPGroup>
+              </InputOTP>
+            </div>
+          </>
+        ) : (
+          <>
+            <Label htmlFor="newPassword">New Password</Label>
+            <Input
+              id="newPassword"
+              type="password"
+              placeholder="New Password"
+              required
+              value={newPassword}
+              onChange={(v) => setNewPassword(v.target.value)}
+              disabled={resetting}
+            />
+          </>
+        )}
       </div>
     </AuthContainer>
   );
