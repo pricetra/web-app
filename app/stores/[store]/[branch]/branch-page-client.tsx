265f1a39e1bd1c3e647ee485e143ad157acdffed
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-02-20 16:39:06 -0600

    feat: show categorized products

--- app/stores/[store]/[branch]/branch-page-client.tsx
+++ app/stores/[store]/[branch]/branch-page-client.tsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { useNavbar } from "@/context/navbar-context";
-import { AllProductsDocument, Branch, Product, Store } from "graphql-utils";
+import {
+  AllProductsDocument,
+  Branch,
+  CategoriesWithProductsDocument,
+  Product,
+  Store,
+} from "graphql-utils";
 import { createCloudinaryUrl } from "@/lib/files";
-import { useLayoutEffect, useMemo } from "react";
-import { useQuery } from "@apollo/client/react";
+import { useEffect, useLayoutEffect, useMemo } from "react";
+import { useLazyQuery } from "@apollo/client/react";
 import ProductItem, { ProductItemLoading } from "@/components/product-item";
 import NavPageIndicator from "@/components/ui/nav-page-indicator";
 import { SmartPagination } from "@/components/ui/smart-pagination";
@@ -22,6 +28,12 @@ import VerticalSidebarAd from "@/components/ads/vertical-sidebar-ad";
 import { uniqueId } from "lodash";
 import BranchPageNavTools from "./components/branch-page-nav-tools";
 import ProductFilterNavToolbar from "@/components/product-filters-nav-toolbar";
+import ScrollContainer from "@/components/scroll-container";
+import ProductItemHorizontal, {
+  ProductLoadingItemHorizontal,
+} from "@/components/product-item-horizontal";
+import HorizontalProductAd from "@/components/ads/horizontal-product-ad";
+import Skeleton from "react-loading-skeleton";
 
 export default function BranchPageClient({
   store,
@@ -53,29 +65,23 @@ export default function BranchPageClient({
     return new URLSearchParams(sp);
   }, [searchParams]);
 
-  const { data: productsData } = useQuery(AllProductsDocument, {
-    fetchPolicy: "no-cache",
-    variables: {
-      paginator: {
-        page: +(searchParams.page ?? 1),
-        limit: +(searchParams.limit ?? 30),
-      },
-      search: {
-        storeId: store.id,
-        branchId: branch.id,
-        query: searchParams.query,
-        brand: searchParams.brand,
-        category: searchParams.category,
-        categoryId: searchParams.categoryId
-          ? +searchParams.categoryId
-          : undefined,
-        sortByPrice: searchParams.sortByPrice,
-        sale: searchParams.sale ? toBoolean(searchParams.sale) : undefined,
-      },
+  const [getCategorizedProducts, { data: categorizedProductsData }] =
+    useLazyQuery(CategoriesWithProductsDocument, {
+      fetchPolicy: "no-cache",
+    });
+  const [getProducts, { data: productsData }] = useLazyQuery(
+    AllProductsDocument,
+    {
+      fetchPolicy: "no-cache",
     },
-  });
+  );
 
   const topHeight = useMemo(() => navbarHeight + 65, [navbarHeight]);
+
+  const paginatorUrlBase = useMemo(
+    () => `/stores/${store.slug}/${branch.slug}`,
+    [store.slug, branch.slug],
+  );
 
   useLayoutEffect(() => {
     if (disableNavSettings) return;
@@ -118,6 +124,46 @@ export default function BranchPageClient({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [branch, disableNavSettings]);
 
+  useEffect(() => {
+    if (paramsBuilder.size > 0) {
+      // Load all products
+      getProducts({
+        variables: {
+          paginator: {
+            page: +(searchParams.page ?? 1),
+            limit: +(searchParams.limit ?? 30),
+          },
+          search: {
+            storeId: store.id,
+            branchId: branch.id,
+            query: searchParams.query,
+            brand: searchParams.brand,
+            category: searchParams.category,
+            categoryId: searchParams.categoryId
+              ? +searchParams.categoryId
+              : undefined,
+            sortByPrice: searchParams.sortByPrice,
+            sale: searchParams.sale ? toBoolean(searchParams.sale) : undefined,
+          },
+        },
+      });
+    } else {
+      getCategorizedProducts({
+        variables: {
+          paginator: {
+            page: +(searchParams.page ?? 1),
+            limit: 10,
+          },
+          productLimit: 20,
+          filters: {
+            branchId: branch.id,
+          },
+        },
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paramsBuilder]);
+
   return (
     <>
       <div className="w-full max-w-[1000px] mt-5 px-5 flex-2">
@@ -127,54 +173,132 @@ export default function BranchPageClient({
           </div>
         )}
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 gap-x-3">
-          {!productsData ? (
-            Array(10)
-              .fill(0)
-              .map((_, j) => (
-                <ProductItemLoading
-                  key={`product-loading-${j}`}
-                  imgWidth={isMobile ? 110 : 130}
-                />
-              ))
-          ) : (
-            <>
-              {productsData.allProducts.paginator.total !== 0 ? (
-                adify(
-                  productsData.allProducts.products,
-                  getRandomIntInclusive(5, 10),
-                ).map((p, i) =>
-                  typeof p === "object" ? (
-                    <ProductItem
-                      product={p as Product}
-                      branchSlug={branch.slug}
-                      imgWidth={isMobile ? 110 : 130}
-                      key={`product-${p.id}-${i}`}
-                      hideStoreInfo
-                    />
-                  ) : (
-                    <VerticalProductAd
-                      id={i}
-                      key={`vertical-product-ad-${p}-${i}`}
-                    />
-                  ),
-                )
-              ) : (
-                <p className="py-10 px-5 text-center">No results</p>
-              )}
-            </>
-          )}
-        </div>
+        {paramsBuilder.size === 0 ? (
+          <div>
+            <div className="flex flex-col">
+              {!categorizedProductsData
+                ? Array(3)
+                    .fill(0)
+                    .map((_, i) => (
+                      <article
+                        className="my-7"
+                        key={`branch-with-product-loading-${i}`}
+                      >
+                        <div className="mb-5 px-5">
+                          <Skeleton width="20%" height={28} borderRadius={10} />
+                        </div>
 
-        {productsData?.allProducts?.paginator &&
-          productsData.allProducts.paginator.numPages > 1 && (
-            <div className="mt-20">
-              <SmartPagination
-                paginator={productsData.allProducts.paginator}
-                urlBase={`/stores/${store.slug}/${branch.slug}`}
-              />
+                        <ScrollContainer hideButtons>
+                          {Array(10)
+                            .fill(0)
+                            .map((_, j) => (
+                              <ProductLoadingItemHorizontal
+                                key={`branch-product-${i}-${j}`}
+                              />
+                            ))}
+                        </ScrollContainer>
+                      </article>
+                    ))
+                : categorizedProductsData.categoriesWithProducts.categories.map(
+                    (category, i) => (
+                      <article
+                        className="my-7"
+                        key={`categorized-products-${category.id}-${i}`}
+                      >
+                        <div className="mb-5 px-5">
+                          <h2 className="text-base xs:text-lg sm:text-xl">
+                            {category.name}
+                          </h2>
+                        </div>
+
+                        <ScrollContainer>
+                          {adify(
+                            category.products ?? [],
+                            getRandomIntInclusive(3, 6),
+                          ).map((product, i) =>
+                            typeof product === "object" ? (
+                              <ProductItemHorizontal
+                                product={product as Product}
+                                branchSlug={branch.slug}
+                                key={`branch-product-${category.id}-${product.id}`}
+                              />
+                            ) : (
+                              <HorizontalProductAd
+                                id={i}
+                                key={`horizontal-product-ad-${category.id}-${product}-${i}`}
+                              />
+                            ),
+                          )}
+                        </ScrollContainer>
+                      </article>
+                    ),
+                  )}
             </div>
-          )}
+
+            {categorizedProductsData?.categoriesWithProducts?.paginator &&
+              categorizedProductsData.categoriesWithProducts.paginator && (
+                <div className="mt-20">
+                  <SmartPagination
+                    paginator={
+                      categorizedProductsData.categoriesWithProducts.paginator
+                    }
+                    urlBase={paginatorUrlBase}
+                  />
+                </div>
+              )}
+          </div>
+        ) : (
+          <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 gap-x-3">
+              {!productsData ? (
+                Array(10)
+                  .fill(0)
+                  .map((_, j) => (
+                    <ProductItemLoading
+                      key={`product-loading-${j}`}
+                      imgWidth={isMobile ? 110 : 130}
+                    />
+                  ))
+              ) : (
+                <>
+                  {productsData.allProducts.paginator.total !== 0 ? (
+                    adify(
+                      productsData.allProducts.products,
+                      getRandomIntInclusive(5, 10),
+                    ).map((p, i) =>
+                      typeof p === "object" ? (
+                        <ProductItem
+                          product={p as Product}
+                          branchSlug={branch.slug}
+                          imgWidth={isMobile ? 110 : 130}
+                          key={`product-${p.id}-${i}`}
+                          hideStoreInfo
+                        />
+                      ) : (
+                        <VerticalProductAd
+                          id={i}
+                          key={`vertical-product-ad-${p}-${i}`}
+                        />
+                      ),
+                    )
+                  ) : (
+                    <p className="py-10 px-5 text-center">No results</p>
+                  )}
+                </>
+              )}
+            </div>
+
+            {productsData?.allProducts?.paginator &&
+              productsData.allProducts.paginator.numPages > 1 && (
+                <div className="mt-20">
+                  <SmartPagination
+                    paginator={productsData.allProducts.paginator}
+                    urlBase={paginatorUrlBase}
+                  />
+                </div>
+              )}
+          </div>
+        )}
       </div>
 
       <div className="w-full px-2 relative flex-1">
