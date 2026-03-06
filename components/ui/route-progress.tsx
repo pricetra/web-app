ca0565ffef0f8837d4f1a90884afb39999484959
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-03-06 05:13:56 -0600

    fix: append current href to route history

--- components/ui/route-progress.tsx
+++ components/ui/route-progress.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
+import { useRouteHistory } from "@/context/route-history";
 import "nprogress/nprogress.css";
 
 NProgress.configure({ showSpinner: false, trickleSpeed: 50 });
@@ -10,9 +11,12 @@ NProgress.configure({ showSpinner: false, trickleSpeed: 50 });
 export default function RouteProgress() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const { addToHistory } = useRouteHistory();
 
   useEffect(() => {
+    addToHistory(window.location.href);
     NProgress.done();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname, searchParams]);
 
   return null;
