af958a59fbd937d952714fb396440e8b636b0029
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-03-06 01:38:08 -0600

    refactor/Remove console.log

--- components/ui/route-progress.tsx
+++ components/ui/route-progress.tsx
@@ -4,21 +4,15 @@ import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import { useRouteHistory } from "@/context/route-history";
 
 NProgress.configure({ showSpinner: false, trickleSpeed: 50 });
 
 export default function RouteProgress() {
-  const { addToHistory, history } = useRouteHistory();
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  useEffect(() => { console.log(history) }, [history])
-
   useEffect(() => {
-    addToHistory(window.location.href);
     NProgress.done();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname, searchParams]);
 
   return null;
