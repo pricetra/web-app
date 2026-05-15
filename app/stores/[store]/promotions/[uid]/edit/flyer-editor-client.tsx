dfc0098bb1e2bc180754c29c8a15ed2ccbfab7f7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-05-15 18:12:08 -0500

    feat: fetch flyer pages

--- app/stores/[store]/promotions/[uid]/edit/flyer-editor-client.tsx
+++ app/stores/[store]/promotions/[uid]/edit/flyer-editor-client.tsx
@@ -3,10 +3,16 @@
 import NavPageIndicator from "@/components/ui/nav-page-indicator";
 import { useNavbar } from "@/context/navbar-context";
 import { createCloudinaryUrl } from "@/lib/files";
-import { StorefrontFlyer, StorefrontFlyerPageInput } from "graphql-utils";
-import { useLayoutEffect, useMemo, useState } from "react";
+import {
+  StorefrontFlyer,
+  StorefrontFlyerDocument,
+  StorefrontFlyerPageInput,
+} from "graphql-utils";
+import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import FlyerPage from "./components/flyer-page";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { useLazyQuery } from "@apollo/client/react";
+import { CgSpinner } from "react-icons/cg";
 
 export type FlyerEditorClientProps = {
   flyer: StorefrontFlyer;
@@ -20,7 +26,17 @@ export default function FlyerEditorClient({ flyer }: FlyerEditorClientProps) {
     setSearchQueryPath,
     navbarHeight,
   } = useNavbar();
+  const [
+    getStorefrontFlyerWithPages,
+    { data: flyerWithPagesData, loading: flyerWithPagesLoading },
+  ] = useLazyQuery(StorefrontFlyerDocument);
   const [pagesInput, setPagesInput] = useState<StorefrontFlyerPageInput[]>([]);
+  const minHeight = useMemo(
+    () => ({
+      minHeight: `calc(100vh - ${navbarHeight}px)`,
+    }),
+    [navbarHeight],
+  );
 
   useLayoutEffect(() => {
     if (!flyer.store) return;
@@ -44,17 +60,49 @@ export default function FlyerEditorClient({ flyer }: FlyerEditorClientProps) {
       resetAll();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [flyer]);
+
+  useEffect(() => {
+    getStorefrontFlyerWithPages({
+      variables: {
+        uid: flyer.uid,
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const minHeight = useMemo(
-    () => ({
-      minHeight: `calc(100vh - ${navbarHeight}px)`,
-    }),
-    [navbarHeight],
-  );
+  useEffect(() => {
+    if (!flyerWithPagesData) return;
+
+    const pages = flyerWithPagesData.storefrontFlyer.pages;
+    const pagesInput = pages.map(
+      (p) =>
+        ({
+          bgImage: p.bgImageId, // TODO: add cloudinary URL
+          description: p.description,
+          heroImage: p.heroImageId, // TODO: add cloudinary URL
+          layout: p.layout,
+          pageImage: p.pageImageId, // TODO: add cloudinary URL
+          sections: p.sections,
+          storefrontFlyerId: p.storefrontFlyerId,
+          styles: p.styles,
+          title: p.title,
+        }) as StorefrontFlyerPageInput,
+    );
+    setPagesInput([...pagesInput]);
+  }, [flyerWithPagesData]);
 
   return (
     <>
+      {/* Overlay */}
+      {flyerWithPagesLoading && (
+        <div className="fixed inset-0 bg-white bg-opacity-70 z-50">
+          <div className="flex items-center justify-center h-full w-full">
+            <CgSpinner className="text-gray-500 text-4xl animate-spin" />
+          </div>
+        </div>
+      )}
+
       <section className="flex-3 bg-gray-50" style={{ ...minHeight }}>
         <div>
           {pagesInput.map((p, index) => (
