e30f6920e02b47ee13b4e891a4cc9d112e7c5fc0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2026-06-25 14:40:27 -0500

    feat: improve select branches segment

--- components/welcome-screen.tsx
+++ components/welcome-screen.tsx
@@ -76,7 +76,7 @@ export default function WelcomeScreen({ user }: WelcomeScreenProps) {
         ),
       },
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [newAddress]);
 
   function submitAddress(address: string) {
@@ -98,9 +98,9 @@ export default function WelcomeScreen({ user }: WelcomeScreenProps) {
   }
 
   return (
-    <div className="flex p-5 flex-col justify-center gap-5 w-full max-w-xl mx-auto min-h-screen">
+    <div className="flex p-5 flex-col justify-center gap-5 w-full mx-auto min-h-screen">
       {page === WelcomePageType.WELCOME && (
-        <div>
+        <div className="max-w-xl mx-auto">
           <div className="w-full flex flex-row justify-start items-center mb-5">
             <Image
               src="/logo_black_color_dark_leaf.svg"
@@ -138,7 +138,7 @@ export default function WelcomeScreen({ user }: WelcomeScreenProps) {
       )}
 
       {page === WelcomePageType.ADDRESS && (
-        <div className="flex flex-col justify-center gap-5">
+        <div className="flex flex-col justify-center gap-5 max-w-xl mx-auto">
           <div className="flex items-center">
             <div className="flex size-[70px] items-center justify-center rounded-full bg-pricetra-green-heavy-dark/10 text-pricetra-green-heavy-dark">
               <GoLocation className="size-8" />
@@ -191,127 +191,139 @@ export default function WelcomeScreen({ user }: WelcomeScreenProps) {
       )}
 
       {page === WelcomePageType.BRANCHES && newAddress && (
-        <div className="flex flex-col justify-center gap-5">
-          <div className="flex items-center">
-            <div className="flex size-[70px] items-center justify-center rounded-full bg-pricetra-green-heavy-dark/10 text-pricetra-green-heavy-dark">
-              <MdStorefront className="size-8" />
+        <div className="flex flex-col sm:flex-row justify-center gap-5 max-w-3xl mx-auto">
+          <div className="flex-1 flex flex-col gap-2">
+            <div className="flex items-center">
+              <div className="flex size-[70px] items-center justify-center rounded-full bg-pricetra-green-heavy-dark/10 text-pricetra-green-heavy-dark">
+                <MdStorefront className="size-8" />
+              </div>
             </div>
+
+            <h2 className="text-2xl sm:text-3xl font-extrabold text-pricetraGreenHeavyDark">
+              Select Nearby Places You Like to Shop
+            </h2>
+
+            <p className="mb-5 text-base sm:text-lg text-gray-800">
+              {newAddress.fullAddress}
+            </p>
           </div>
 
-          <h2 className="text-2xl sm:text-3xl font-extrabold text-pricetraGreenHeavyDark">
-            Select Nearby Places You Like to Shop
-          </h2>
+          <div className="flex-2">
+            <div className="rounded-lg bg-gray-50 border border-gray-200 relative">
+              <div className="p-5">
+                {branchesLoading && (
+                  <div className="flex flex-row items-center justify-center px-5 py-14">
+                    <CgSpinner className="animate-spin size-10" />
+                  </div>
+                )}
+                {branchesData?.findBranchesByDistance?.length === 0 && (
+                  <div>
+                    <p className="mb-3 text-lg font-semibold">
+                      There aren&apos;t any stores located near you
+                    </p>
 
-          <p className="mb-5 text-base sm:text-lg text-gray-800">
-            {newAddress.fullAddress}
-          </p>
-
-          <div>
-            <div className="rounded-lg bg-gray-50 p-5">
-              {branchesLoading && (
-                <div className="flex flex-row items-center justify-center px-5 py-14">
-                  <CgSpinner className="animate-spin size-10" />
-                </div>
-              )}
-              {branchesData?.findBranchesByDistance?.length === 0 && (
-                <div>
-                  <p className="mb-3 text-lg font-semibold">
-                    There aren&apos;t any stores located near you
-                  </p>
-
-                  <p>
-                    We are constantly adding new stores and will alert you when
-                    new locations are added.
-                  </p>
-                </div>
-              )}
-              {branchesData &&
-                branchesData.findBranchesByDistance.map((branch) => (
-                  <div
-                    onClick={() => {
-                      const foundBranch = selectedBranches.find(
-                        ({ id }) => id === branch.id,
-                      );
-                      if (foundBranch) {
-                        setSelectedBranches((branches) =>
-                          branches.filter(({ id }) => id !== branch.id),
+                    <p>
+                      We are constantly adding new stores and will alert you
+                      when new locations are added.
+                    </p>
+                  </div>
+                )}
+                {branchesData &&
+                  branchesData.findBranchesByDistance.map((branch) => (
+                    <div
+                      onClick={() => {
+                        const foundBranch = selectedBranches.find(
+                          ({ id }) => id === branch.id,
                         );
-                        return;
-                      }
-                      setSelectedBranches((branches) => {
-                        const newArr = branches.filter(
-                          ({ id }) => id !== branch.id,
-                        );
-                        newArr.push(branch as Branch);
-                        return newArr;
-                      });
-                    }}
-                    className="mb-7 flex flex-row justify-between cursor-pointer"
-                    key={branch.id}
-                  >
-                    <div className="flex flex-1 flex-row items-center gap-2">
-                      <Image
-                        src={createCloudinaryUrl(
-                          branch.store?.logo ?? "",
-                          100,
-                          100,
-                          startOfNextSundayUTC(),
+                        if (foundBranch) {
+                          setSelectedBranches((branches) =>
+                            branches.filter(({ id }) => id !== branch.id),
+                          );
+                          return;
+                        }
+                        setSelectedBranches((branches) => {
+                          const newArr = branches.filter(
+                            ({ id }) => id !== branch.id,
+                          );
+                          newArr.push(branch as Branch);
+                          return newArr;
+                        });
+                      }}
+                      className="mb-7 flex flex-row justify-between cursor-pointer"
+                      key={branch.id}
+                    >
+                      <div className="flex flex-1 flex-row items-center gap-2">
+                        <Image
+                          src={createCloudinaryUrl(
+                            branch.store?.logo ?? "",
+                            100,
+                            100,
+                            startOfNextSundayUTC(),
+                          )}
+                          width={100}
+                          height={100}
+                          quality={100}
+                          alt={branch.name}
+                          className="size-[35px] rounded-lg"
+                        />
+                        <div>
+                          <h4 className="text font-semibold">
+                            {branch.store?.name}
+                          </h4>
+                          <p className="text-xs">
+                            {branch.address?.fullAddress}
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row items-center justify-center">
+                        {selectedBranches.find(
+                          ({ id }) => id === branch.id,
+                        ) && (
+                          <MdCheck className="size-6 text-pricetra-green-heavy-dark" />
                         )}
-                        width={100}
-                        height={100}
-                        quality={100}
-                        alt={branch.name}
-                        className="size-[35px] rounded-lg"
-                      />
-                      <div>
-                        <h4 className="text font-semibold">
-                          {branch.store?.name}
-                        </h4>
-                        <p className="text-xs">{branch.address?.fullAddress}</p>
                       </div>
                     </div>
+                  ))}
+              </div>
 
-                    <div className="flex flex-row items-center justify-center">
-                      {selectedBranches.find(({ id }) => id === branch.id) && (
-                        <MdCheck className="size-6 text-pricetra-green-heavy-dark" />
-                      )}
-                    </div>
-                  </div>
-                ))}
+              <div className="bg-white sticky bottom-0 border-gray-200 border-t rounded-b-lg">
+                <div className="flex flex-row items-center gap-3 px-5 py-3 justify-between">
+                  <Button
+                    variant="outline"
+                    disabled={addingBranches}
+                    onClick={() => setPage(WelcomePageType.ADDRESS)}
+                  >
+                    Back
+                  </Button>
+
+                  <Button
+                    variant="pricetra"
+                    disabled={selectedBranches.length === 0 || addingBranches}
+                    onClick={async () => {
+                      setAddingBranches(true);
+                      addBranchesToList({
+                        variables: {
+                          listId: lists!.favorites.id,
+                          branchIds: selectedBranches.map(({ id }) => id),
+                        },
+                      }).then(({ data }) => {
+                        if (!data)
+                          return window.alert(
+                            "There was an error while adding the selected branches to your favorites list. Please try again.",
+                          );
+                        setAddingBranches(false);
+                      });
+                    }}
+                  >
+                    {addingBranches && <CgSpinner className="animate-spin" />}
+                    Finish Setup
+                  </Button>
+                </div>
+              </div>
             </div>
 
-            <div className="flex flex-row items-center gap-3 mt-10 justify-between">
-              <Button
-                variant="outline"
-                disabled={addingBranches}
-                onClick={() => setPage(WelcomePageType.ADDRESS)}
-              >
-                Back
-              </Button>
-
-              <Button
-                variant="pricetra"
-                disabled={selectedBranches.length === 0 || addingBranches}
-                onClick={async () => {
-                  setAddingBranches(true);
-                  addBranchesToList({
-                    variables: {
-                      listId: lists!.favorites.id,
-                      branchIds: selectedBranches.map(({ id }) => id),
-                    },
-                  }).then(({ data }) => {
-                    if (!data)
-                      return window.alert(
-                        "There was an error while adding the selected branches to your favorites list. Please try again.",
-                      );
-                    setAddingBranches(false);
-                  });
-                }}
-              >
-                {addingBranches && <CgSpinner className="animate-spin" />}
-                Finish Setup
-              </Button>
-            </div>
+            <div style={{ height: "10vh" }} />
           </div>
         </div>
       )}
